08d63b591e034a9e078c1d8188219ad984f6ee05
lgm527 <laurellmccaffrey@gmail.com>
2019-07-15 18:45:36 -0400

    MVC walked through

--- app.rb
+++ app.rb
@@ -7,7 +7,12 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @count_of_words = @analyzed_text.count_of_words
+    @count_of_vowels = @analyzed_text.count_of_vowels
+    @count_of_consonants = @analyzed_text.count_of_consonants
+    @most_used_letter = @analyzed_text.most_used_letter.keys.join.capitalize
+    @most_used_letter_count = @analyzed_text.most_used_letter.values.join
 
     erb :results
   end
